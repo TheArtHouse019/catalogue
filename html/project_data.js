ce3231e139461f363018acb489004a833865ad20
naresh263 <deoxys263@gmail.com>
2026-07-17 22:51:11 +0530

    sanika art house

---
+++ html/project_data.js
@@ -0,0 +1,22 @@
+var project_data = [
+    {
+        "project_name":"ay",
+        "logo":"533",
+        "url":"533",
+        "organization":"533",
+        "description":"533",
+        'screenshots':{
+            0:{"name":'name',"image":'image'},
+            1:{"name":'name',"image":'image'},
+            2:{"name":'name',"image":'image'},
+            4:{"name":'name',"image":'image'},
+            5:{"name":'name',"image":'image'},
+            
+        }
+
+    },
+{"name":"kiy","id":"33"},
+{"name":"iy","id":"33"},
+{"name":"iy","id":"3"},
+{"name":"kiy","id":"35"},
+{"name":"kiy","id":"34"}];
